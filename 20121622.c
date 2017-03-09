6eab4332691aaac8fd660b7db4c46349e120781a
undergraduate <cse20121622@cspro6.sogang.ac.kr>
2017-03-09 04:55:59 -0800

    make sicsim

---
+++ 20121622.c
@@ -0,0 +1,9 @@
+#include "header.h"
+
+int main() {
+	while (1) {
+		char input[100];
+		sicsim();
+		scanf("%s", input);
+	}
+}
---
+++ function.c
@@ -0,0 +1,7 @@
+#include "header.h"
+
+void sicsim();
+
+void sicsim() {
+	printf("%s>", "sicsim");
+}

585c0406a8664a4bb1a3773cefa81b2b85aa7921
undergraduate <cse20121622@cspro6.sogang.ac.kr>
2017-03-09 05:41:43 -0800

    dir

--- function.c
+++ function.c
@@ -3,6 +3,7 @@
 void sicsim();
 void selector(char input[]);
 void help();
+void dir();
 
 void sicsim() {
 	printf("%s>", "sicsim");
@@ -10,6 +11,8 @@ void sicsim() {
 void selector(char input[]) {
 	if (!strcmp(input, "help") || !strcmp(input, "h")) {
 		help();
+	} else if (!strcmp(input, "dir") || !strcmp(input, "d")) {
+		dir();
 	}
 }
 void help() {
@@ -24,3 +27,35 @@ void help() {
 	printf("opcode mnemonic\n");
 	printf("opcodelist\n");
 }
+
+void dir() {
+	DIR *dir;
+	struct dirent *direntp;
+
+	dir = opendir(".");
+	
+	while ((direntp = readdir(dir)) != NULL) {
+		char fileName[256];
+		int returnStat;
+		struct stat fileInfo;
+		mode_t fileMode;
+		
+		strcpy(fileName, direntp->d_name);
+		if ((returnStat = stat(fileName, &fileInfo)) ==-1) {
+			perror("Error : ");
+			exit(0);
+		}
+		fileMode = fileInfo.st_mode;
+		printf("%s", fileName);
+		if (S_ISDIR(fileMode)) {
+			printf("/");
+		} else if (fileMode & S_IXUSR || fileMode & S_IXGRP || fileMode & S_IXOTH) {
+			printf("*");
+		}
+		puts("");
+	}
+	closedir(dir);
+}
+//reference
+//https://linux.die.net/man/2/stat
+//http://pubs.opengroup.org/onlinepubs/7908799/xsh/sysstat.h.html
--- header.h
+++ header.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 void sicsim();
 void selector();
 void help();
+void dir();
